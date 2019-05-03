791059954768b2c95f259b104e506f5542fae97c
iamatran <iamatran@hotmail.com>
2019-05-02 20:50:17 -0500

    added home to modal

--- Social Media Project/src/pages/modal/modal.ts
+++ Social Media Project/src/pages/modal/modal.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, ViewController } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { AccountingServiceProvider } from '../../providers/accounting-service/accounting-service';
+
 
 /**
  * Generated class for the ModalPage page.
@@ -15,8 +20,75 @@ import { IonicPage, ViewController } from 'ionic-angular';
 })
 export class ModalPage {
 
-  constructor(private view: ViewController) {
+  constructor(private view: ViewController, public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController, public dataService: AccountingServiceProvider) {
   }
+
+
+  loadItems() {
+    return this.dataService.getItems();
+  }
+
+  removeItem(item, index) {
+    console.log("Removing Item - ", item, index);
+    const toast = this.toastCtrl.create({
+        message: "Removing Item - " + item.name + " ..." ,
+        duration: 3000
+    });
+    toast.present();
+
+    this.dataService.removeItem(index);
+  }
+
+
+  addItem() {
+    console.log('meow');
+    this.showAddItemPrompt();
+  }
+
+
+  showAddItemPrompt() {
+    const prompt = this.alertCtrl.create({
+      title: 'Add a new post',
+      message: "Please enter your post details...",
+      inputs: [
+        {
+          name: 'name',
+          value: 'Name'
+        },
+        {
+          name: 'date',
+          value: 'March 26, 2019'
+        },
+        {
+          name: 'description',
+          value: 'Omg, this app is so awesome!!!'
+        },
+        {
+          name: 'avatar',
+          value: '../../assets/imgs/advance-card-bttf.png'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: item => {
+            console.log('Saved clicked', item);
+            this.dataService.addItem(item);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+
+
 
   closeModal(){
     this.view.dismiss();
