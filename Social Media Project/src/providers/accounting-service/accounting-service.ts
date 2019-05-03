d20f652faea1a89f224d35b7ba8d3a099c73e5f4
iamatran <iamatran@hotmail.com>
2019-05-03 03:40:50 -0500

    many updates
    all json data updated
    styling format
    pics
    modal not working

--- Social Media Project/src/providers/accounting-service/accounting-service.ts
+++ Social Media Project/src/providers/accounting-service/accounting-service.ts
@@ -11,26 +11,70 @@ export class AccountingServiceProvider {
 
   items = [
     {
-      name: "Milk",
-      date: "March 26, 2019",
-      description: "Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy",
-      comments: 4,
-      avatar: "../../assets/imgs/marty-avatar.png",
-      picture: "../../assets/imgs/advance-card-bttf.png",
+      name: "Steve Rogers",
+      date: "May 3, 2019",
+      description: "The film is expected to gross over 2 BILLION DOLLARS before the weekend is over.. INSANE.",
+      comments: 27,
+      avatar: "../../assets/imgs/a1.jpg",
+      picture: "../../assets/imgs/p1.jpg",
       modalName:"Reliant K Swiss",
       modalComment:"It's Chapstick and Chapped Lips and Things Like Chemistry",
-      modalAvatar: "../../assets/imgs/marty-avatar.png"
+      modalAvatar: "../../assets/imgs/ma1.png"
     },
     {
-      name: "Candy",
+      name: "Jenna Morningstarr",
       date: "March 26, 2019",
-      description: "Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy",
-      comments: 27,
-      avatar: "../../assets/imgs/marty-avatar.png",
-      picture: "../../assets/imgs/advance-card-bttf.png",
+      description: "My timeline’s filled with Avengers & GOT vids and memes like thanks, guys but i really miss my cats, dogs, and other funny stuff",
+      comments: 6,
+      avatar: "../../assets/imgs/a2.jpg",
+      picture: "",
+      modalName: "Mickey Sousburger",
+      modalComment: "Wow, I really can't beleive it's really been that long!",
+      modalAvatar: "../../assets/imgs/ma2.jpg"
+    },
+    {
+      name: "This is fine.",
+      date: "March 26, 2019",
+      description: "This is literally every student studying during finals week:",
+      comments: 13,
+      avatar: "../../assets/imgs/a3.jpg",
+      picture: "../../assets/imgs/p3.jpg",
+      modalName: "Noregian Greek",
+      modalComment: "Have you seen that one thing where you climb up the mountain only to get swatted down?",
+      modalAvatar: "../../assets/imgs/ma3.jpg"
+    },
+    {
+      name: "Compadre Instant",
+      date: "January 3, 2019",
+      description: "I was there last night. Wow. Amazing women of Birmingham are making a huge difference! #ShopWithLove is where I'm buying all my Chrimbo Pressies!",
+      comments: 7,
+      avatar: "../../assets/imgs/a4.jpg",
+      picture: "",
       modalName: "Nappy Boy Radio",
-      modalComment: "She want that Lovey Dovey and Kiss Kiss",
-      modalAvatar: "../../assets/imgs/marty-avatar.png"
+      modalComment: "Me: I understood that reference Everyone else: ...",
+      modalAvatar: "../../assets/imgs/ma4.jpg"
+    },
+    {
+      name: "Oliver Queen",
+      date: "March 8, 2019",
+      description: "Great film! Will see it again! Only wish, there were more fights with the entire crew back together....possibly chasing Thanos over different worlds and in the daytime to visualise more....as the other Avengers movies were filmed in. That said, thanks and keep up the great work.",
+      comments: 2,
+      avatar: "../../assets/imgs/a5.jpg",
+      picture: "",
+      modalName: "Hamster Dash",
+      modalComment: "Why hasen't she come yet? The graduation is right around the corner!",
+      modalAvatar: "../../assets/imgs/ma5.jpg"
+    },
+    {
+      name: "Beastie Boyz",
+      date: "April 11, 2019",
+      description: "(That stressed me out, wow) But YES finally seeing my faves Half Moon Run in November!",
+      comments: 21,
+      avatar: "../../assets/imgs/a6.jpg",
+      picture: "../../assets/imgs/p6.jpg",
+      modalName: "Nuff Said N.",
+      modalComment: "How come this kind of stuff only happens to me?!",
+      modalAvatar: "../../assets/imgs/ma6.jpg"
     },
 
 
